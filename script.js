364c48613589925545ea3f58fc461efc407362d6
spector-armstrong <franklinluvv@gmail.com>
2025-12-01 21:17:24 +0300

    added modal

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const gameCanvas = document.querySelector(".canvas");
 const undo = document.querySelector(".undo");
 const restart = document.querySelector(".restart");
 const table = document.querySelector(".table");
+let currScore = document.querySelector(".currScore");
+let highScore = document.querySelector(".highScore");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
-gameCanvas.style.height = "750px";
-gameCanvas.style.width = "750px";
+gameCanvas.style.height = "760px";
+gameCanvas.style.width = "760px";
 
 const cellColor = {
     2: "#f3e5ff",
@@ -18,4 +22,24 @@ const cellColor = {
     512: "#2d0052",
     1024: "#11001f",
     2048: "#000000",
-}
+};
+const emptyCell = "#e4e2cd";
+const cellSize = "190px";
+
+undo.addEventListener("click", function () {
+    none;
+});
+
+restart.addEventListener("click", function () {
+    currScore.textContent = 0;
+});
+
+table.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+});
+
+document.querySelector(".close-modal").addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
